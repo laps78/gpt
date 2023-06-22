63ed8204d9d2beddb9a7797c3f24af19cfa919d7
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-06-23 00:22:04 +0300

    install.sh modified: ascii arts adjusted, console reports upgraded

--- dist/install.sh
+++ dist/install.sh
@@ -5,21 +5,22 @@ br="\n\n"
 
 # initial actions
 echo "Запуск скрипта установщика бота на сервер ubuntu..."
-echo "                                             "
-echo " (              (    (       (               "
-echo " )\ )    (      )\ ) )\ )    )\ )         )  "
-echo "(()/(    )\    (()/((()/(   (()/(    ) ( /(  "
-echo " /(_))((((_)(   /(_))/(_))   /(_))( /( )\()) "
-echo "(_))   )\ _ )\ (_)) (_))    (_))  )(_)|(_)\  "
-echo "| |    (_)_\(_)| _ \/ __|   | |  ((_)_| |(_) "
-echo "| |__ _ / _ \ _|  _/\__ \_  | |__/ _\'| '_ \ "
-echo "|____(_)_/ \_(_)_|(_)___(_) |____\__,_|_.__/ "
-echo "                                             "
-echo "L.A.P.S. GPT TELEGRAM BOT ATOMATIC INSTALLER FOR SPRINTBOX v0.1:$br init..."
+echo "***********************************************************************"
+echo "* ______   _______   ________  ________        ______        ______   *"
+echo "* ___  /   ___    |  ___  __ \ __  ___/        ___  / ______ ___  /_  *"
+echo "* __  /    __  /| |  __  /_/ / _____ \         __  /  _  __  /_  __ \ *"
+echo "* _  /______  ___ |___  ____/______/ /__       _  /___/ /_/ /_  /_/ / *"
+echo "* /_____/(_)_/  |_|(_)_/    _(_)____/_(_)      /_____/\__,_/ /_.___/  *"
+echo "*                                                                     *"
+echo "***********************************************************************"
+
+
+echo "L.A.P.S. GPT TELEGRAM BOT ATOMATIC INSTALLER FOR SPRINTBOX v0.1:"
+echo "init..."
 
 # create bot user
 echo "Будет создан пользователь gpt_bot. Вам будет предложено ввести и подтвердить UNIX пароль, а также заполнить дополнительную информацию о пользователе. Обязательно требуется точно ввести и повторить пароль, остальные данные - просто нажимайте enter";
-adduser gpt_bot
+adduser gpt_bot && echo "Пользователь gpt_bot создан"
 
 # install packages
 sudo apt update && sudo apt upgrade -y
@@ -33,38 +34,34 @@ virtualenv --system-site-packages python
 source ~/python/bin/activate
 
 # install libraries
-pip install openai telebot datetime
-echo "Требуемые модули библиотек python подключены."
+pip install openai telebot datetime && echo "Требуемые модули библиотек python подключены."
 
 # clone project
 git clone https://github.com/laps78/gpt && cd gpt # TODO change project repo configuration
 
 # create env & set api tokens
 touch .env
-
-echo "                                 _____ "  
-echo "________________________________ ___(_)"
-echo "_  __ \__  __ \  _ \_  __ \  __ `/_  / "              
-echo "/ /_/ /_  /_/ /  __/  / / / /_/ /_  /  "             
-echo "\____/_  .___/\___//_/ /_/\__,_/ /_/   "            
-echo "      /_/                              "  
-echo "***************************************"
+echo "                                 _____  "  
+echo "________________________________ ___(_) "
+echo "_  __ \__  __ \  _ \_  __ \  __  /_  /  "              
+echo "/ /_/ /_  /_/ /  __/  / / / /_/ /_  /   "             
+echo "\____/_  .___/\___//_/ /_/\__,_/ /_/    "            
+echo "      /_/                               "  
+echo "****************************************"
 echo "Введите токен, полученный на сайте openai.com:"
 read OPENAI_TOKEN
 echo ""
-echo "_____     ______                                      "
-echo "__  /________  /___________ _____________ _______ ___ "
-echo "_  __/  _ \_  /_  _ \_  __ `/_  ___/  __ `/_  __ `__ \"
-echo "/ /_ /  __/  / /  __/  /_/ /_  /   / /_/ /_  / / / / /"
-echo "\__/ \___//_/  \___/_\__, / /_/    \__,_/ /_/ /_/ /_/ "
-echo "                    /____/                            "
-echo "******************************************************"
-
+echo "_____     ______                                       "
+echo "__  /________  /___________ _____________ _______ ___  "
+echo "_  __/  _ \_  /_  _ \_  __  /_  ___/  __  /_  __  __ \ "
+echo "/ /_ /  __/  / /  __/  /_/ /_  /   / /_/ /_  / / / / / "
+echo "\__/ \___//_/  \___/_\__, / /_/    \__,_/ /_/ /_/ /_/  "
+echo "                    /____/                             "
+echo "*******************************************************"
 echo "Введите токен, полученный в Telegram от @botFather:"
 read TG_TOKEN
-echo "OPENAI_TOKEN=$OPENAI_TOKEN" > .env
-echo "TG_TOKEN=$TG_TOKEN" >> .env
-echo "Переменные окружения установлены."
+echo "OPENAI_TOKEN=$OPENAI_TOKEN" > .env && echo "openai токен установлен"
+echo "TG_TOKEN=$TG_TOKEN" >> .env && echo "telegram токен установен" && echo "Переменные окружения установлены."
 
 # install watchdog daemon systemctl service
 cat > /etc/systemd/system/laps-gpt-bot.service << EOF
@@ -76,8 +73,8 @@ After=network.target
 [Service]
 Type=simple
 User=gpt_bot
-WorkingDirectory=/root/gpt
-ExecStart=/usr/bin/python3 /root/gpt/gpt-bot.py
+WorkingDirectory=/home/gpt
+ExecStart=/usr/bin/python3 /home/gpt/gpt-bot.py
 Restart=always
 
 [Install]
